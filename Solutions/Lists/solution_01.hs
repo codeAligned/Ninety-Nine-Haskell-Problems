79013769abd46c547cca50ac03cc33ae2662ff99
Nam V. Do <vannam12a7@gmail.com>
2019-08-21 09:26:57 +0700

    Update solution_01.hs

--- Solutions/Lists/solution_01.hs
+++ Solutions/Lists/solution_01.hs
@@ -1,1 +1,22 @@
+
+myLast :: [a] -> a
+myLast [] = error "No end for empty lists!"
+myLast [x] = x
+myLast (_:xs) = myLast xs
+
+myLast' = foldr1 (const id)
+
+-- Prelude> const 1 2
+-- 1
+-- Prelude> (flip const) 1 2
+-- 2
+myLast'' = foldr1 (flip const)
+
+myLast''' = head . reverse
+
+myLast'''' = foldl1 (curry snd)
+
+myLast''''' [] = error "No end for empty lists!"  
+myLast''''' x = x !! (length x -1)
+-- The Prelude also provides the function last.
 last [1, 2, 3, 4]
